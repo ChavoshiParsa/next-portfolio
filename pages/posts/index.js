675a7b7551ae69538115710e04f6d81313be46c3
Your Parsa <parypary82@gmail.com>
2023-07-31 22:28:49 +0330

    i dont know what the heck going on there?

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,40 @@
+import AllPosts from "@/components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-it SSR.",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs1",
+  },
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-it SSR.",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs2",
+  },
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-it SSR.",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs3",
+  },
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-it SSR.",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs4",
+  },
+];
+
 export default function AllPostsPage(props) {
-  return <></>;
+  return <AllPosts posts={DUMMY_POSTS} />;
 }
